20a04d0d3a738abab720bff00878d2d393a3d2a5
Toke Høiland-Jørgensen <toke@toke.dk>
2018-10-23 17:57:12 +0200

    Add logos to title slide as well
    Signed-off-by: Toke Høiland-Jørgensen <toke@toke.dk>

--- conference/LinuxPlumbers2018/reveal.js/js/custom.js
+++ conference/LinuxPlumbers2018/reveal.js/js/custom.js
@@ -9,9 +9,9 @@ var authorHTML = [
     '<i class="icon-cc-1"></i>',
     '<i class="icon-cc-by"></i>',
     '<i class="icon-cc-sa"></i>',
-    '</a> - ',
+    '</a><span class="authors"> - ',
     authorName, ' &lt;', authorEmail, '&gt; &amp;&nbsp;',
-    authorName2, ' &lt;', authorEmail2, '&gt; &nbsp;'].join('');
+    authorName2, ' &lt;', authorEmail2, '&gt; &nbsp;</span>'].join('');
 
 function addAuthor(selector) {
     return function() {
@@ -42,5 +42,6 @@ if (window.location.search.match( /print-pdf/gi )) {
     window.addEventListener('pdf-ready', hideFake('.pdf-page'));
 } else {
     window.addEventListener('load', addAuthor('.reveal .slides > section > section'));
+    window.addEventListener('load', addAuthor('#sec-title-slide'));
 }
 window.addEventListener('DOMContentLoaded', hideFake('section'));
