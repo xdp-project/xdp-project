c1d5f028d6844215ad594f1ed4f8b9c78c6f29ca
Jesper Dangaard Brouer <brouer@redhat.com>
2018-10-25 16:43:36 +0200

    Add copy of ./reveal.js/js/custom.js
    Need to costimized for other talk with Andy.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ conference/LinuxPlumbers2018/reveal.js/js/custom-xdp-tutorial.js
@@ -0,0 +1,47 @@
+var authorName = 'Jesper Dangaard Brouer';
+var authorName2 = 'Toke Høiland-Jørgensen';
+var authorEmail = 'brouer@redhat.com';
+var authorEmail2 = 'toke@toke.dk';
+
+var authorHTML = [
+    '<img src="./reveal.js/images/logo.svg" />',
+    '<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">',
+    '<i class="icon-cc-1"></i>',
+    '<i class="icon-cc-by"></i>',
+    '<i class="icon-cc-sa"></i>',
+    '</a><span class="authors"> - ',
+    authorName, ' &lt;', authorEmail, '&gt; &amp;&nbsp;',
+    authorName2, ' &lt;', authorEmail2, '&gt; &nbsp;</span>'].join('');
+
+function addAuthor(selector) {
+    return function() {
+        var elems = document.querySelectorAll(selector);
+        elems.forEach(function (e) {
+            var author = document.createElement('div');
+            author.className = 'authorbox';
+            author.innerHTML = authorHTML;
+            e.appendChild(author);
+        });
+    }
+}
+
+function hideFake(selector) {
+    return function() {
+        var elems = document.querySelectorAll(selector);
+        elems.forEach(function (e) {
+            console.log(e.getAttribute('fake'));
+            if (e.getAttribute('fake')) {
+                e.parentElement.removeChild(e);
+            }
+        });
+    }
+}
+
+
+if (window.location.search.match( /print-pdf/gi )) {
+    window.addEventListener('pdf-ready', hideFake('.pdf-page'));
+} else {
+    window.addEventListener('load', addAuthor('.reveal .slides > section > section'));
+    window.addEventListener('load', addAuthor('#sec-title-slide'));
+}
+window.addEventListener('DOMContentLoaded', hideFake('section'));
