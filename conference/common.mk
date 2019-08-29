fb5177a018abf29bd925bb0ab3ade44d60bdb283
Toke Høiland-Jørgensen <toke@redhat.com>
2019-08-29 14:22:51 +0200

    conference: Auto-detect decktape.js for PDF building
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- conference/common.mk
+++ conference/common.mk
@@ -2,7 +2,9 @@
 CONF_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 BUILD_SCRIPT := $(CONF_DIR)emacs.d/build-slides.sh
 BUILD_PDF_SCRIPT := $(CONF_DIR)build-pdf.sh
-DECKTAPE := decktape --chrome-arg=--no-sandbox
+
+DECKTAPE_JS := $(wildcard ~/git/decktape/decktape.js)
+DECKTAPE := $(if $(DECKTAPE_JS), node $(DECKTAPE_JS), decktape --chrome-arg=--no-sandbox)
 
 
 HTML_FILES := ${ORG_FILES:.org=.html}
