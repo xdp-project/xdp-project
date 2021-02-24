f420b9b1549b47fba7ec71a4f6c89039eefe8e09
Toke Høiland-Jørgensen <toke@redhat.com>
2021-02-24 14:25:03 +0100

    conference/FOSDEM2019: Move to common reveal.js copy
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- conference/reveal.js/js/custom-fosdem2019.js
+++ conference/reveal.js/js/custom-fosdem2019.js
@@ -1,10 +1,29 @@
+var titleFooter = 'XDP as a building block for other FOSS projects'
 var authorName = 'Jesper Dangaard Brouer';
 var authorEmail = 'brouer@redhat.com';
 
+var authorName2 = 'Magnus Karlsson';
+var authorEmail2 = "magnus.karlsson@intel.com";
+
 var authorHTML = [
-    '<img src="./reveal.js/images/RedHat.svg" />',
-    '<span class="authors"> - ',
-    authorName, ' &lt;', authorEmail, '&gt; &nbsp;</span>'].join('');
+    '<img src="../reveal.js/images/RedHat.svg" />',
+    '<span class="footer">',
+    '&nbsp;&nbsp;&nbsp;',
+    '&nbsp;&nbsp;&nbsp;',
+    '&nbsp;&nbsp;&nbsp;',
+    '&nbsp;&nbsp;&nbsp;',
+    '&nbsp;&nbsp;&nbsp;',
+    '&nbsp;&nbsp;&nbsp;',
+    titleFooter,
+    '</span>',
+    '<span class="authors">',
+    authorName,
+    '&nbsp;', '&amp;&nbsp;',
+    authorName2,
+    '</span>',
+    '&nbsp;&nbsp;&nbsp;',
+    '<img src="../reveal.js/images/intel-logo-vector-01.png" />',
+].join('');
 
 function addAuthor(selector) {
     return function() {
@@ -19,12 +38,14 @@ function addAuthor(selector) {
 }
 
 /* needs to be in an onLoad callback, otherwise hljs is not defined yet */
+/*
 function addHighlightKeywords() {
         hljs.getLanguage('C').k.keyword += ' XDP_DROP XDP_PASS XDP_ABORTED XDP_REDIRECT XDP_TX';
         hljs.getLanguage('C').k.keyword += ' data data_end';
         hljs.getLanguage('C').k.keyword += ' xdp_do_redirect xdp_do_flush_map';
 }
+window.addEventListener('load', addHighlightKeywords);
+*/
 
 window.addEventListener('load', addAuthor('.reveal .slides > section > section'));
 window.addEventListener('load', addAuthor('#sec-title-slide'));
-window.addEventListener('load', addHighlightKeywords);
